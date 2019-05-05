cf4825afe7d0e0c8fb247201aa045d770e57cb73
Jesse Evans <jesseelizabeth1@gmail.com>
2019-05-05 15:06:58 -0400

    added icons to tab navigation!

--- client/components/TabNavigator.js
+++ client/components/TabNavigator.js
@@ -1,13 +1,40 @@
+/* eslint-disable react/display-name */
+import React from 'react';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
 import Home from './Home';
 import MyWines from './MyWines';
 import Camera from './Camera';
 
-const TabNavigator = createBottomTabNavigator({
-  Home: Home,
-  Camera: Camera,
-  myWines: MyWines,
-});
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: Home,
+    Camera: Camera,
+    myWines: MyWines,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = 'ios-wine';
+        } else if (routeName === 'Account') {
+          iconName = 'md-person';
+        } else if (routeName === 'Camera') {
+          iconName = 'ios-camera';
+        }
+
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
+    },
+  }
+);
 
 const AppContainer = createAppContainer(TabNavigator);
 
