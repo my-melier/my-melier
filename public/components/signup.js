d8a112ba89a3a16e4972f723a47f40fbcad12c54
Larkin <larkindsmith@gmail.com>
2019-05-05 12:33:02 -0400

    three components, no routes yet

---
+++ public/components/signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Signup Page Goes Here!</Text>
+      </View>
+    )
+  }
+}
