d5a71604d16649375253d5c04d46ab17872e4615
Jesse Evans <jesseelizabeth1@gmail.com>
2019-05-08 17:03:23 -0400

    network error issue, thinking it has to do with crypto

--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', require('./api'));
 app.use('/auth', require('./auth'));
 
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
 db.sync().then(() => {
   app.listen(8080, () => console.log('listening on port 8080'));
 });
