5670a826fea0874252f24b7cbb4bd03581f05832
Aly Norrgard <amnorrgard@gmail.com>
2019-05-07 20:53:32 -0400

    created comparisonReducer and combined it with rootReducer

---
+++ client/store/reducers/comparisonReducer.js
@@ -0,0 +1,46 @@
+//action types
+const ADDING_TO_COMPARISONS = 'ADDING_TO_COMPARISONS';
+const ADDED_TO_COMPARISONS = 'ADDED_TO_COMPARISONS';
+const SELECTED_WINE = 'SELECTED_WINE';
+
+//action creators
+export const addingToComparisons = () => ({
+  type: ADDING_TO_COMPARISONS,
+});
+
+export const addedToComparisons = wine => ({
+  type: ADDED_TO_COMPARISONS,
+  wine,
+});
+
+export const selectedWine = wine => ({
+  type: SELECTED_WINE,
+  wine,
+});
+
+//thunks
+
+//initial state
+const initialState = {
+  loading: false,
+  comparisons: [],
+  selectedWine: {},
+};
+
+//reducer
+export default (comparisonReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADDING_TO_COMPARISONS:
+      return { ...state, loading: true };
+    case ADDED_TO_COMPARISONS:
+      return {
+        ...state,
+        comparisons: [...state.comparisons, action.wine],
+        loading: false,
+      };
+    case SELECTED_WINE:
+      return { ...state, selectedWine: action.wine };
+    default:
+      return state;
+  }
+});
--- client/store/reducers/index.js
+++ client/store/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import googleVisionReducer from './googleVisionReducer';
 import dbReducer from './dbReducer';
+import comparisonReducer from './comparisonReducer';
 
 const rootReducer = combineReducers({
   googleVision: googleVisionReducer,
   database: dbReducer,
+  comparisons: comparisonReducer,
 });
 
 export default rootReducer;
