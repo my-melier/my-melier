daefa6b0ac6d181b162e70f57e2d74917a2d11f8
Aly Norrgard <amnorrgard@gmail.com>
2019-05-07 21:46:50 -0400

    created Comparisons component

---
+++ client/components/Comparisons.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Text, StyleSheet, View, ScrollView, Button } from 'react-native';
+import { connect } from 'react-redux';
+import selectedWine from '../store/reducers/comparisonReducer'
+
+export default class Comparisons extends Component {
+  render() {
+      const {comparisons, selectedWine} = this.props;
+      const { navigate } = this.props.navigation;
+
+    return (
+      <ScrollView>
+        <View style={styles.container}>
+          <Text> myMenu </Text>
+          <View style={styles.container}>
+            {comparisons.map(wine => {
+                return(
+                    <View>
+                        <Text>{wine.title}</Text>
+                        <Text>{wine.description}</Text>
+                        <Text>{wine.score}</Text>
+                        <Button title="Select" onPress={() => selectedWine(wine)}/>
+                    </View>
+                )
+            })}
+          </View>
+            <Button title="Add another wine from menu" onPress={() => navigate('Camera')} />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
+
+const mapState = state => ({
+    comparisons: state.comparisons.comparisons
+})
+
+const mapDispatch = dispatch => ({
+    selectedWine: (wine) => dispatch(selectedWine(wine))
+})
+
+export default connect(mapState, mapDispatch)(Comparisons)
