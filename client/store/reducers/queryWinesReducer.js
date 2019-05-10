27969787a78083039e708349d305aa39a2caa227
Jesse Evans <jesseelizabeth1@gmail.com>
2019-05-10 12:45:42 -0400

    sign up login working

--- client/store/reducers/queryWinesReducer.js
+++ client/store/reducers/queryWinesReducer.js
@@ -1,59 +1,59 @@
-import axios from 'axios'
-import myIPaddress from '../../../IPaddress'
+import axios from 'axios';
+import myIPaddress from '../../../IPaddress';
 
 //action types
-const GETTING_WINES = 'GETTING_WINES'
-const GOT_WINES = 'GOT_WINES'
+const GETTING_WINES = 'GETTING_WINES';
+const GOT_WINES = 'GOT_WINES';
 
 //action creators
 export const gettingWines = () => ({
-  type: GETTING_WINES
-})
+  type: GETTING_WINES,
+});
 
 export const gotWines = wines => ({
   type: GOT_WINES,
-  wines
-})
+  wines,
+});
 
 //thunks
 export const fetchingWinesFromDb = googleResFormatted => async dispatch => {
   try {
-    dispatch(gettingWines())
-    const {data} = await axios.get(
+    dispatch(gettingWines());
+    const { data } = await axios.get(
       `http://${myIPaddress.IP}:8080/api/wine/${googleResFormatted}`
-    )
-    dispatch(gotWines(data))
+    );
+    dispatch(gotWines(data));
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
 //initial state
 const initialState = {
   loading: false,
-  results: []
-}
+  results: [],
+};
 
 //reducer
 export default (state = initialState, action) => {
   switch (action.type) {
     case GETTING_WINES:
-      return {...state, loading: true}
+      return { ...state, loading: true };
     case GOT_WINES:
-      let uniqueValues = []
-      let uniqueTitles = []
+      let uniqueValues = [];
+      let uniqueTitles = [];
       for (let i = 0; i < action.wines.length; i++) {
         if (!uniqueTitles.includes(action.wines[i].title)) {
-          uniqueTitles.push(action.wines[i].title)
-          uniqueValues.push(action.wines[i])
+          uniqueTitles.push(action.wines[i].title);
+          uniqueValues.push(action.wines[i]);
         }
       }
       return {
         ...state,
         results: uniqueValues,
-        loading: false
-      }
+        loading: false,
+      };
     default:
-      return state
+      return state;
   }
-}
+};
--- server/index.js
+++ server/index.js
@@ -1,63 +1,63 @@
-const express = require('express')
-const {db} = require('./db')
-const app = express()
-const passport = require('passport')
-const session = require('express-session')
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
-const sessionStore = new SequelizeStore({db})
+const express = require('express');
+const { db } = require('./db');
+const app = express();
+const passport = require('passport');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sessionStore = new SequelizeStore({ db });
 
-module.exports = app
+module.exports = app;
 
-passport.serializeUser((user, done) => done(null, user.id))
+passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await db.models.user.findByPk(id)
-    done(null, user)
+    const user = await db.models.user.findByPk(id);
+    done(null, user);
   } catch (err) {
-    done(err)
+    done(err);
   }
-})
+});
 
 const createApp = () => {
-  app.use(express.json())
-  app.use(express.urlencoded({extended: true}))
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
   app.use(
     session({
       secret: process.env.SESSION_SECRET || 'my best friend is Cody',
       store: sessionStore,
       resave: false,
-      saveUninitialized: false
+      saveUninitialized: false,
     })
-  )
+  );
 
-  app.use(passport.initialize())
-  app.use(passport.session())
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-  app.use('/api', require('./api'))
-  app.use('/auth', require('./auth'))
+  app.use('/api', require('./api'));
+  app.use('/auth', require('./auth'));
 
   app.use((err, req, res, next) => {
-    console.error(err)
-    console.error(err.stack)
-    res.status(err.status || 500).send(err.message || 'Internal server error.')
-  })
-}
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+  });
+};
 
 const startListening = () => {
-  app.listen(8080, () => console.log('listening on port 8080'))
-}
+  app.listen(8080, () => console.log('listening on port 8080'));
+};
 
-const syncDb = () => db.sync()
+const syncDb = () => db.sync();
 
 async function bootApp() {
-  await sessionStore.sync()
-  await syncDb()
-  await createApp()
-  await startListening()
+  await sessionStore.sync();
+  await syncDb();
+  await createApp();
+  await startListening();
 }
 
-bootApp()
+bootApp();
 
 // db.sync().then(() => {
 //   app.listen(8080, () => console.log('listening on port 8080'))
