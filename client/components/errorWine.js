8f1ee574a56733da82822452cf9aec4e217149b6
Aly Norrgard <amnorrgard@gmail.com>
2019-05-13 11:16:46 -0400

    deleted duplicate errorWine component that wasn't deleted from separate branch

---
+++ client/components/errorWine.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react'
+import {Text, StyleSheet, View, Image, ScrollView, Button} from 'react-native'
+
+export default class ErrorWine extends Component {
+  render() {
+    const noWine = {
+      uri: 'https://i.gifer.com/GPkz.gif'
+    }
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>
+          Oh no! We don't have that particular wine in our cellar. Please try
+          another wine!
+        </Text>
+        <View style={styles.gifView}>
+          <Image source={noWine} style={styles.gif} />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20
+  },
+  message: {
+    textAlign: 'center',
+    fontSize: 45,
+    padding: 20
+  },
+  gif: {
+    width: 375,
+    height: 275
+  },
+  gifView: {
+    alignItems: 'center',
+    paddingBottom: 20,
+    paddingTop: 20
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 30
+  }
+})
