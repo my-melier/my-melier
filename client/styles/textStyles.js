9dac78ea3ec1557addfc51f841bde04462e68ab6
Jesse Evans <jesseelizabeth1@gmail.com>
2019-05-15 13:46:21 -0400

    styles

--- client/styles/textStyles.js
+++ client/styles/textStyles.js
@@ -1,20 +1,38 @@
 import { StyleSheet } from 'react-native';
 
 const textStyles = StyleSheet.create({
-  h1: {
+  logo: {
     fontSize: 50,
     textAlign: 'center',
+    padding: 5,
+  },
+  h1: {
+    fontSize: 40,
+    textAlign: 'center',
+    padding: 5,
   },
   h2: {
     fontSize: 30,
     textAlign: 'center',
+    padding: 5,
   },
   h3: {
-    fontSize: 24,
+    fontSize: 18,
     textAlign: 'center',
+    padding: 5,
   },
   h4: {},
-  h5: {},
+  h5: {
+    fontSize: 16,
+    textAlign: 'justify',
+    padding: 5,
+  },
+  h5bold: {
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    padding: 5,
+  },
   bold: {
     fontWeight: 'bold',
   },
